ca6584bcabfa4d6e5516af4d04f7e935047a54a3
yoyofoe1 <yoyofoe1@outlook.com>
2017-04-06 16:25:57 -0400

    Update MiningHelp.java

--- MiningHelp/src/com/techbotbuilder/mininghelp/MiningHelp.java
+++ MiningHelp/src/com/techbotbuilder/mininghelp/MiningHelp.java
@@ -26,12 +26,12 @@ final class MiningHelpListener implements Listener {
   public MiningHelpListener(){
 	super();
     lootOptions = new HashMap<Material, ItemStack>();
-    lootOptions.put(Material.STONE_PICKAXE, new ItemStack(Material.IRON_INGOT, 32));
+    lootOptions.put(Material.STONE_PICKAXE, new ItemStack(Material.IRON_INGOT, 10));
     lootOptions.put(Material.WOOD_PICKAXE, new ItemStack(Material.COBBLESTONE, 64));
-    lootOptions.put(Material.IRON_PICKAXE, new ItemStack(Material.GOLD_INGOT, 10));
-    lootOptions.put(Material.GOLD_PICKAXE, new ItemStack(Material.DIAMOND, 2));
-    lootOptions.put(Material.DIAMOND_PICKAXE, new ItemStack(Material.OBSIDIAN, 32));
-    lootOptions.put(Material.DIAMOND_SWORD, new ItemStack(Material.TNT, 32));
+    lootOptions.put(Material.IRON_PICKAXE, new ItemStack(Material.GOLD_INGOT, 5));
+    lootOptions.put(Material.GOLD_PICKAXE, new ItemStack(Material.DIAMOND, 1));
+    lootOptions.put(Material.DIAMOND_PICKAXE, new ItemStack(Material.OBSIDIAN, 16));
+    lootOptions.put(Material.DIAMOND_SWORD, new ItemStack(Material.TNT, 16));
   }
     @EventHandler
     public void playerRightClick(PlayerInteractEvent event) {
