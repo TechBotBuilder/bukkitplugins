2f25095970c43c8f711827c6c1debb7391623b2c
Maxwell Budd <techbotbuilder@gmail.com>
2017-04-23 17:33:51 -0400

    Safe-ify

--- NetherUpgrade/src/com/techbotbuilder/netherupgrade/NetherUpgrade.java
+++ NetherUpgrade/src/com/techbotbuilder/netherupgrade/NetherUpgrade.java
@@ -1,5 +1,29 @@
 package com.techbotbuilder.netherupgrade;
 
+import static org.bukkit.Material.ANVIL;
+import static org.bukkit.Material.BEACON;
+import static org.bukkit.Material.BED_BLOCK;
+import static org.bukkit.Material.BREWING_STAND;
+import static org.bukkit.Material.CAKE_BLOCK;
+import static org.bukkit.Material.CHEST;
+import static org.bukkit.Material.DISPENSER;
+import static org.bukkit.Material.DRAGON_EGG;
+import static org.bukkit.Material.ENCHANTMENT_TABLE;
+import static org.bukkit.Material.ENDER_CHEST;
+import static org.bukkit.Material.FLOWER_POT;
+import static org.bukkit.Material.FURNACE;
+import static org.bukkit.Material.HOPPER;
+import static org.bukkit.Material.JUKEBOX;
+import static org.bukkit.Material.LEVER;
+import static org.bukkit.Material.MINECART;
+import static org.bukkit.Material.NOTE_BLOCK;
+import static org.bukkit.Material.STORAGE_MINECART;
+import static org.bukkit.Material.TRAPPED_CHEST;
+import static org.bukkit.Material.TRAP_DOOR;
+import static org.bukkit.Material.WORKBENCH;
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.Set;
 
 import org.bukkit.Location;
@@ -10,7 +34,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.ItemStack;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -24,7 +47,7 @@ public class NetherUpgrade extends JavaPlugin {
 final class LightningSummoningListener implements Listener {
     @EventHandler
     public void playerRightClick(PlayerInteractEvent event) {
-        if (event.hasItem() && event.getItem().getType() == Material.BLAZE_ROD){
+        if (event.hasItem() && event.getItem().getType() == Material.BLAZE_ROD && isNiceClick(event)){
           Player player = event.getPlayer();
           Block target = player.getTargetBlock((Set<Material>)null,100);
           Location targetlocation = target.getLocation();
@@ -32,4 +55,24 @@ final class LightningSummoningListener implements Listener {
           world.strikeLightning(targetlocation);
         }
     }
+    public static boolean isNiceClick(PlayerInteractEvent e){
+		if(e.hasBlock() && !e.getPlayer().isSneaking()){
+			List<Material> derps = Arrays.asList(
+					ENCHANTMENT_TABLE, ANVIL, BREWING_STAND,
+					NOTE_BLOCK, TRAPPED_CHEST, BED_BLOCK, JUKEBOX,
+					CHEST, ENDER_CHEST, FURNACE, HOPPER, BEACON,
+					DISPENSER, CAKE_BLOCK, DRAGON_EGG, FLOWER_POT,
+					MINECART, STORAGE_MINECART, TRAP_DOOR,
+					WORKBENCH, LEVER);
+			List<String> superDerps = Arrays.asList("door", "gate", "boat", "diode",
+					"comparator");
+			Material m = e.getClickedBlock().getType();
+			String ms = m.getData().getName();
+			for(String s : superDerps){
+				if(ms.equalsIgnoreCase("org.bukkit.material."+s)) return false;
+			}
+			if(derps.contains(m)) return false;
+		}
+		return true;
+	}
 }
