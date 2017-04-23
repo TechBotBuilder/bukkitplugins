0931fc3900d202a81eaf499626bf6f332b7b9f64
Maxwell Budd <techbotbuilder@gmail.com>
2017-04-23 15:10:12 -0400

    Take items

--- BridgeBuilding/src/cc/atspace/yoyofoe1/bridgebuilding/BridgeBuilding.java
+++ BridgeBuilding/src/cc/atspace/yoyofoe1/bridgebuilding/BridgeBuilding.java
@@ -4,21 +4,28 @@ import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Location;
 import org.bukkit.Material;
- 
+
 public class BridgeBuilding extends JavaPlugin {
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
     if (command.getName().equalsIgnoreCase("bridge")){
       if (sender instanceof Player){
         Player player = (Player) sender;
-        Location playerbottomblock = player.getLocation().subtract(2, 1, 2);
-        for (int z=0; z<5; z++){
-          for (int x=0; x<5; x++){
-            Block allblock = playerbottomblock.clone().add(x, 0, z).getBlock();
-            allblock.setType(Material.COBBLESTONE);
+        Location playerbottomblock = player.getLocation().subtract(2, 1, 2);;
+        if (player.getInventory().containsAtLeast(new ItemStack(Material.COBBLESTONE), 25)) {
+          removeInventoryItems(player.getInventory(), Material.COBBLESTONE, 25);
+          for (int z=0; z<5; z++){
+            for (int x=0; x<5; x++){
+              Block allblock = playerbottomblock.clone().add(x, 0, z).getBlock();
+              allblock.setType(Material.COBBLESTONE);
+            }
           }
+        }else{
+          sender.sendMessage("You need at least 25 Cobblestone in your inventory for this command to work!");
         }
       }else{
         return false;
@@ -26,5 +33,20 @@ public class BridgeBuilding extends JavaPlugin {
       return true;
     }
     return false;
+  }
+  public static void removeInventoryItems(PlayerInventory inv, Material type, int amount) {
+    for (ItemStack is : inv.getContents()) {
+        if (is != null && is.getType() == type) {
+            int newamount = is.getAmount() - amount;
+            if (newamount > 0) {
+                is.setAmount(newamount);
+                break;
+            } else {
+                inv.remove(is);
+                amount = -newamount;
+                if (amount == 0) break;
+            }
+        }
+    }
   }
 }
