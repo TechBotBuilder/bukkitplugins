3c7167577824f3b0b25bae58c86fc0a9472981b4
Maxwell Budd <techbotbuilder@gmail.com>
2017-04-23 14:59:46 -0400

    Golden Shovel activation

--- CubeRemover/src/cc/atspace/yoyofoe1/cuberemover/CubeRemover.java
+++ CubeRemover/src/cc/atspace/yoyofoe1/cuberemover/CubeRemover.java
@@ -4,6 +4,10 @@ import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -16,32 +20,7 @@ public class CubeRemover extends JavaPlugin {
     if (command.getName().equalsIgnoreCase("cuberemove")){
       if (sender instanceof Player){
         Player player = (Player) sender;
-        Location playerbottomblock = player.getLocation();
-        Vector direction = player.getLocation().getDirection();
-        Vector mainDirection = getPrincipleDirection(direction);
-        Vector sideDirection1 = getSideDirection1(mainDirection);
-        Vector sideDirection2 = getSideDirection2(mainDirection);
-        if (removeInventoryItems(player.getInventory(), Material.COAL, 2)) {
-          for (int mainDirectionStep=1; mainDirectionStep<=5; mainDirectionStep++){
-            for (int sideDirection1Step=-2; sideDirection1Step<=2; sideDirection1Step++){
-              for (int sideDirection2Step=-2; sideDirection2Step<=2; sideDirection2Step++){
-                Vector mainDirectionJump = mainDirection.clone().multiply(mainDirectionStep);
-                Vector sideDirection1Jump = sideDirection1.clone().multiply(sideDirection1Step);
-                Vector sideDirection2Jump = sideDirection2.clone().multiply(sideDirection2Step);
-                Vector jump = new Vector();
-                jump.add(mainDirectionJump);
-                jump.add(sideDirection1Jump);
-                jump.add(sideDirection2Jump);
-                Block allblock = playerbottomblock.clone().add(jump).getBlock();
-                if (allblock.getType() == Material.STONE || allblock.getType() == Material.DIRT  || allblock.getType() == Material.GRASS){
-                  allblock.setType(Material.AIR);
-                }
-              }
-            }
-          }
-          }else{
-            sender.sendMessage("You need at least 2 Coal in your inventory for this command to work!");
-          }
+        cubeRemove(player);
       }else{
         return false;
       }
@@ -96,4 +75,46 @@ public class CubeRemover extends JavaPlugin {
 		  return new Vector(0, 0, z/v.getZ());
 	  }
   }
+  public static void cubeRemove(Player player){
+	  Location playerbottomblock = player.getLocation();
+      Vector direction = player.getLocation().getDirection();
+      Vector mainDirection = getPrincipleDirection(direction);
+      Vector sideDirection1 = getSideDirection1(mainDirection);
+      Vector sideDirection2 = getSideDirection2(mainDirection);
+      if (removeInventoryItems(player.getInventory(), Material.COAL, 2)) {
+        for (int mainDirectionStep=1; mainDirectionStep<=5; mainDirectionStep++){
+          for (int sideDirection1Step=-2; sideDirection1Step<=2; sideDirection1Step++){
+            for (int sideDirection2Step=-2; sideDirection2Step<=2; sideDirection2Step++){
+              Vector mainDirectionJump = mainDirection.clone().multiply(mainDirectionStep);
+              Vector sideDirection1Jump = sideDirection1.clone().multiply(sideDirection1Step);
+              Vector sideDirection2Jump = sideDirection2.clone().multiply(sideDirection2Step);
+              Vector jump = new Vector();
+              jump.add(mainDirectionJump);
+              jump.add(sideDirection1Jump);
+              jump.add(sideDirection2Jump);
+              Block allblock = playerbottomblock.clone().add(jump).getBlock();
+              if (allblock.getType() == Material.STONE || allblock.getType() == Material.DIRT  || allblock.getType() == Material.GRASS){
+                allblock.setType(Material.AIR);
+              }
+            }
+          }
+        }
+        }else{
+          player.sendMessage("You need at least 2 Coal in your inventory to use CubeRemover!");
+        }
+  }
 }
+
+class ShovelListener implements Listener {
+	@EventHandler
+	public void shovelRemove(PlayerInteractEvent e){
+		if (e.hasItem() && e.getItem().getType()==Material.GOLD_SPADE
+				&& (e.getAction()==Action.RIGHT_CLICK_AIR
+						|| e.getAction()==Action.RIGHT_CLICK_BLOCK))
+		{
+			CubeRemover.cubeRemove(e.getPlayer());
+		}
+	}
+}
+
+
