c5f88b517df9a994b40d1c69a631c92534d55e22
yoyofoe1 <yoyofoe1@outlook.com>
2017-04-27 07:39:47 -0400

    Add golden pick click

--- BridgeBuilding/src/cc/atspace/yoyofoe1/bridgebuilding/BridgeBuilding.java
+++ BridgeBuilding/src/cc/atspace/yoyofoe1/bridgebuilding/BridgeBuilding.java
@@ -32,8 +32,37 @@ public class BridgeBuilding extends JavaPlugin {
       }
       return true;
     }
+    if (command.getName().equalsIgnoreCase("wall")){
+      if (sender instanceof Player){
+        Player player = (Player) sender;
+        Location playerbottomblock = player.getLocation().subtract(2, 1, 2);;
+        if (player.getInventory().containsAtLeast(new ItemStack(Material.COBBLESTONE), 25)) {
+          removeInventoryItems(player.getInventory(), Material.COBBLESTONE, 25);
+          for (int z=0; z<5; z++){
+            for (int x=0; x<5; x++){
+              Block allblock = playerbottomblock.clone().add(x, 0, z).getBlock();
+              allblock.setType(Material.COBBLESTONE);
+            }
+          }
+        }else{
+          sender.sendMessage("You need at least 25 Cobblestone in your inventory for this command to work!");
+        }
+      }else{
+        return false;
+      }
+      return true;
+    }
     return false;
   }
+  class PickaxeListener implements Listener {
+	@EventHandler
+	public void pickaxeRemove(PlayerInteractEvent e){
+		if ( !e.isCancelled() && e.hasItem() && e.getItem().getType()==Material.GOLD_PICKAXE
+				&& isNiceRightClick(e))
+		{
+			BridgeBuilding.bridge(e.getPlayer());
+		}
+	}
   public static void removeInventoryItems(PlayerInventory inv, Material type, int amount) {
     for (ItemStack is : inv.getContents()) {
         if (is != null && is.getType() == type) {
@@ -49,4 +78,27 @@ public class BridgeBuilding extends JavaPlugin {
         }
     }
   }
+  	public static boolean isNiceRightClick(PlayerInteractEvent e){
+		if( !(e.getAction()==Action.RIGHT_CLICK_AIR
+				|| e.getAction()==Action.RIGHT_CLICK_BLOCK)) return false;
+		if(e.hasBlock() && !e.getPlayer().isSneaking()){
+			List<Material> derps = Arrays.asList(
+					ENCHANTMENT_TABLE, ANVIL, BREWING_STAND,
+					NOTE_BLOCK, TRAPPED_CHEST, BED_BLOCK, JUKEBOX,
+					CHEST, ENDER_CHEST, FURNACE, HOPPER, BEACON,
+					DISPENSER, CAKE_BLOCK, DRAGON_EGG, FLOWER_POT,
+					MINECART, STORAGE_MINECART, TRAP_DOOR,
+					WORKBENCH, LEVER, GOLD_BLOCK);
+			List<String> superDerps = Arrays.asList("door", "gate", "boat", "diode",
+					"comparator");
+			Material m = e.getClickedBlock().getType();
+			String ms = m.getData().getName();
+			for(String s : superDerps){
+				if(ms.equalsIgnoreCase("org.bukkit.material."+s)) return false;
+			}
+			if(derps.contains(m)) return false;
+		}
+		return true;
+	}
+}
 }
