929dc1c2b1c95f65aff2d24bca70d59656071c8e
yoyofoe1 <yoyofoe1@outlook.com>
2017-04-29 15:08:22 -0400

    added directional building

--- BridgeBuilding/src/cc/atspace/yoyofoe1/bridgebuilding/BridgeBuilding.java
+++ BridgeBuilding/src/cc/atspace/yoyofoe1/bridgebuilding/BridgeBuilding.java
@@ -12,58 +12,45 @@ import org.bukkit.Material;
 
 public class BridgeBuilding extends JavaPlugin {
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
-    if (command.getName().equalsIgnoreCase("bridge")){
-      if (sender instanceof Player){
-        Player player = (Player) sender;
-        Location playerbottomblock = player.getLocation().subtract(2, 1, 2);;
-        if (player.getInventory().containsAtLeast(new ItemStack(Material.COBBLESTONE), 25)) {
-          removeInventoryItems(player.getInventory(), Material.COBBLESTONE, 25);
-          for (int z=0; z<5; z++){
-            for (int x=0; x<5; x++){
-              Block allblock = playerbottomblock.clone().add(x, 0, z).getBlock();
-              allblock.setType(Material.COBBLESTONE);
-            }
-          }
-        }else{
-          sender.sendMessage("You need at least 25 Cobblestone in your inventory for this command to work!");
-        }
-      }else{
-        return false;
-      }
-      return true;
-    }
     if (command.getName().equalsIgnoreCase("wall")){
       if (sender instanceof Player){
         Player player = (Player) sender;
-        Location playerbottomblock = player.getLocation().subtract(2, 1, 2);;
-        if (player.getInventory().containsAtLeast(new ItemStack(Material.COBBLESTONE), 25)) {
-          removeInventoryItems(player.getInventory(), Material.COBBLESTONE, 25);
-          for (int z=0; z<5; z++){
-            for (int x=0; x<5; x++){
-              Block allblock = playerbottomblock.clone().add(x, 0, z).getBlock();
-              allblock.setType(Material.COBBLESTONE);
-            }
-          }
-        }else{
-          sender.sendMessage("You need at least 25 Cobblestone in your inventory for this command to work!");
-        }
-      }else{
-        return false;
+        bridge(player);
+	return true;
       }
-      return true;
     }
     return false;
   }
-  class PickaxeListener implements Listener {
-	@EventHandler
-	public void pickaxeRemove(PlayerInteractEvent e){
-		if ( !e.isCancelled() && e.hasItem() && e.getItem().getType()==Material.GOLD_PICKAXE
-				&& isNiceRightClick(e))
-		{
-			BridgeBuilding.bridge(e.getPlayer());
-		}
+  public static void bridge(Player player){
+      Location playerbottomblock = player.getLocation();
+      Vector direction = player.getLocation().getDirection();
+      Vector mainDirection = getPrincipleDirection(direction);
+      Vector sideDirection1 = getSideDirection1(mainDirection);
+      Vector sideDirection2 = getSideDirection2(mainDirection);
+      if (removeInventoryItems(player.getInventory(), Material.COBBLESTONE, 25)) {
+        for (int mainDirectionStep=1; mainDirectionStep<=1; mainDirectionStep++){
+          for (int sideDirection1Step=-2; sideDirection1Step<=2; sideDirection1Step++){
+            for (int sideDirection2Step=-2; sideDirection2Step<=2; sideDirection2Step++){
+              Vector mainDirectionJump = mainDirection.clone().multiply(mainDirectionStep);
+              Vector sideDirection1Jump = sideDirection1.clone().multiply(sideDirection1Step);
+              Vector sideDirection2Jump = sideDirection2.clone().multiply(sideDirection2Step);
+              Vector jump = new Vector();
+              jump.add(mainDirectionJump);
+              jump.add(sideDirection1Jump);
+              jump.add(sideDirection2Jump);
+              Block allblock = playerbottomblock.clone().add(jump).getBlock();
+              allblock.setType(Material.COBBLESTONE);
+            }
+          }
+        }
+      }else{
+        player.sendMessage("You need at least 25 Cobblestone in your inventory for this command to work!");
+      }
+  }
+  public static boolean removeInventoryItems(PlayerInventory inv, Material type, int amount) {
+	if (!inv.containsAtLeast(new ItemStack(type), amount)){
+		return false;
 	}
-  public static void removeInventoryItems(PlayerInventory inv, Material type, int amount) {
     for (ItemStack is : inv.getContents()) {
         if (is != null && is.getType() == type) {
             int newamount = is.getAmount() - amount;
@@ -77,7 +64,46 @@ public class BridgeBuilding extends JavaPlugin {
             }
         }
     }
+    return true;
   }
+  public static Vector getSideDirection1(Vector v){
+    if (Math.abs(v.getX()) == 1){ //1, 0, 0
+      return new Vector(0, 1, 0);
+    }else{
+      return new Vector(1, 0, 0);
+    }
+  }
+  public static Vector getSideDirection2(Vector v){
+    if (Math.abs(v.getX()) == 1){
+      return new Vector(0, 0, 1);
+    }else{
+      if (Math.abs(v.getY()) == 1){
+        return new Vector(0, 0, 1);
+      }else{
+        return new Vector(0, 1, 0);
+    }
+    }
+  }
+  public static Vector getPrincipleDirection(Vector v){
+	  double x = Math.abs(v.getX()), y=Math.abs(v.getY()), z=Math.abs(v.getZ());
+	  if(x >= y && x > z){
+		  return new Vector(x/v.getX(), 0, 0);
+	  }else if (y > z){
+		  return new Vector(0, y/v.getY(), 0);
+	  }else{
+		  return new Vector(0, 0, z/v.getZ());
+	  }
+  }
+}
+  class PickaxeListener implements Listener {
+	@EventHandler
+	public void pickaxeRemove(PlayerInteractEvent e){
+		if ( !e.isCancelled() && e.hasItem() && e.getItem().getType()==Material.GOLD_PICKAXE
+				&& isNiceRightClick(e))
+		{
+			BridgeBuilding.bridge(e.getPlayer());
+		}
+	}
   	public static boolean isNiceRightClick(PlayerInteractEvent e){
 		if( !(e.getAction()==Action.RIGHT_CLICK_AIR
 				|| e.getAction()==Action.RIGHT_CLICK_BLOCK)) return false;
